5493938d32ec3ee9796f67565fcf5817dfce9d27
golesuman <gole35830@gmail.com>
2022-06-27 19:49:33 +0545

    added views

---
+++ Authentication/authentication/auth_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .serializers import PostSerializer
+from rest_framework import generics
+from .models import Post
+
+# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+
